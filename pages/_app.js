a1415f0799ab4340da274d3fbe7031b6cf939a11
leonardofbastos <leof_bastos@hotmail.com>
2025-07-10 13:29:59 -0300

    Ajuste: deploy realizado

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-fossa-2025.png" />
+        <title>Meu Site</title>
+      </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
